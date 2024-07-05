79a4b65ebf927be373d136b99c77dd95246346cd
Arthika A T <117513536+atarthika@users.noreply.github.com>
2024-07-05 07:21:09 +0000

    first commit

--- Javascript-Basics-Day1/Array.js
+++
@@ -1,3 +0,0 @@
-//camelCase
-let myArray = [1, "two", { name: "John", age: 20 }, [4, 5, 6]];
-document.write(myArray);
--- Javascript-Basics-Day1/caseSensitive.js
+++
@@ -1,8 +0,0 @@
-//Case Sensitive
-
-let sum = 23;
-let Sum = 32;
-let SUM = 10;
-document.write("sum: " + sum + "<br/>");
-document.write("Sum: " + Sum + "<br/>");
-document.write("SUM: " + SUM + "<br/>");
